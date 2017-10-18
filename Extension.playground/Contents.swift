84de844859b860dfbe04a6c2f5448e1afe8e5976
ReasonAmu <luongvantiem@gmail.com>
2017-10-18 23:19:09 +0700

    extension Nib

---
+++ Extension.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension UIView {
+    class func fromNib<T : UIView>() -> T {
+        return Bundle.main.loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+    
+    static var nib:UINib {
+        return UINib(nibName: identifier , bundle: nil)
+    }
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
